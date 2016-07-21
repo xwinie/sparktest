88f834b7fd10603789df97035c37a15860f5521c
xiongshibo@chuangjia.me <xiongshibo@chuangjia.me>
2016-07-21 16:33:37 +0800

    修改

--- src/main/scala/DataFrameParquetFile.scala
+++ src/main/scala/DataFrameParquetFile.scala
@@ -8,7 +8,7 @@ object DataFrameParquetFile {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("TestUDF").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val sqlContext = new SQLContext(sc)
     val row1 = Row("Bruce Zhang", "developer", 38 )
     val row2 = Row("Zhang Yi", "engineer", 39)
     val table = List(row1, row2)
